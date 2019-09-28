4bf851d7109db23bb804ed5ca37e21871be07197
Driss Boumlik <boumlik.t@gmail.com>
2019-09-28 17:31:13 +0100

    Add header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {LoginService} from '../login/login.service';
+import {Router} from '@angular/router';
+import { User } from '../user/user.model';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  searchForm: FormGroup;
+  userImage: string;
+  username: string;
+
+  constructor(private loginService: LoginService,
+              private router: Router) { }
+
+
+  ngOnInit() {
+    let logged = localStorage.getItem('_user') !== null && localStorage.getItem('_user') !== undefined;
+    if(logged) {
+      let user = JSON.parse(localStorage.getItem('_user')) as User;
+      this.userImage = user.picture;
+      this.username = user.name;
+    }
+    this.searchForm = new FormGroup({
+      q: new FormControl(null)
+    });
+  }
+  onLogout() {
+    this.loginService.logout();
+    this.loginService.logoutEvent.subscribe(
+      () => this.router.navigate(['/'])
+    );
+  }
+}
