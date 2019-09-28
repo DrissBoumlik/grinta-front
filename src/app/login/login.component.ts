86e43044461dbd7899ad7fc3029ab73300ec7005
Driss Boumlik <boumlik.t@gmail.com>
2019-09-28 17:30:50 +0100

    Add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,27 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginService} from './login.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import { User } from '../user/user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  providers: [LoginService]
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User;
   loginForm = new FormGroup({
-    username: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required)
+    username: new FormControl('gennaro60@example.net', Validators.required),
+    password: new FormControl('123123123', Validators.required)
   });
   constructor(private loginService: LoginService,
-              private router: Router) { }
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let userLogged = localStorage.getItem('_token') !== null && localStorage.getItem('_token') !== undefined;
+    if(userLogged)
+      this.router.navigate(['/user']);
   }
 
   onLogin() {
-    this.loginService.login();
-    this.router.navigate(['/user']);
+    this.loginService.login(this.loginForm.value.username, this.loginForm.value.password);
+    this.loginService.loginEvent.subscribe(
+      (user) => {
+        this.router.navigate(['user'], { relativeTo: this.route });
+      }
+    );
   }
 }
