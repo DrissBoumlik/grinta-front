5502840f3ee6443c6747f040a7c08d8c6fa417d3
Driss Boumlik <boumlik.t@gmail.com>
2019-09-30 12:11:08 +0100

    Add friend component

---
+++ src/app/user/friend/friend.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../user.model';
+
+@Component({
+  selector: 'app-friend',
+  templateUrl: './friend.component.html',
+  styleUrls: ['./friend.component.css']
+})
+export class FriendComponent implements OnInit {
+  @Input() friend: User;
+  constructor() { }
+  //* TODO: Responsive styles for friends list
+  ngOnInit() {
+  }
+
+}
