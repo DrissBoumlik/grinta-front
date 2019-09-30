0997435bc337e5c07e27b8665babf290efe1f20a
Driss Boumlik <boumlik.t@gmail.com>
2019-09-30 12:17:03 +0100

    Add shared error class - environment variables

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,10 +1,13 @@
-import {User} from '../user/user.model';
-import {Subject} from 'rxjs';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { map } from "rxjs/operators";
 import { Injectable } from '@angular/core';
-@Injectable()
+import {Subject} from 'rxjs';
+import { environment } from './../../environments/environment';
 
+import { Error } from './../shared/error';
+import {User} from '../user/user.model';
+import { map } from "rxjs/operators";
+
+@Injectable()
 export class LoginService {
   public user: User;
   headers = new HttpHeaders({'Content-Type': 'application/json'});
@@ -12,12 +15,14 @@ export class LoginService {
   logoutEvent = new Subject();
   // private http: HttpClient = new HttpClient();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.user = JSON.parse(localStorage.getItem('_user')) as User;
+  }
 
   login(username: string, password: string) {
     let img = 'http://media-assets.dv/images/think-twice-code-once-2560x1600.jpg';
 
-    this.http.post('http://127.0.0.1:8000/api/login', JSON.stringify({email: username, password: password}), {headers: this.headers})
+    this.http.post(environment.baseUrl + '/login', JSON.stringify({email: username, password: password}), {headers: this.headers})
       .toPromise()
       .then((response: any) => {
         localStorage.setItem('_token', response.success.token);
@@ -25,7 +30,7 @@ export class LoginService {
         this.user = response.success.user;
         this.loginEvent.next(this.user);
       })
-      .catch(this.handleError);
+      .catch(Error.handleError);
   }
 
   logout() {
@@ -35,16 +40,12 @@ export class LoginService {
       'Content-Type': 'application/json',
       "Authorization": 'Bearer ' + localStorage.getItem('_token'), Accept: "application/json"
     });
-    this.http.get('http://127.0.0.1:8000/api/logout', {headers: headers})
+    this.http.get(environment.baseUrl + '/logout', {headers: headers})
     .toPromise()
     .then(() => {
       this.logoutEvent.next();
-    });
-  }
-
-  handleError(error: any): Promise<any> {
-    console.error('An error occurred', error); // for demo purposes only
-    return Promise.reject(error.message || error);
+    })
+    .catch(Error.handleError);
   }
 
 }
