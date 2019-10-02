4ea17e8d53f2da7ba531ea233136097d57c626c3
Driss Boumlik <boumlik.t@gmail.com>
2019-10-03 00:56:42 +0100

    Add friends component + Move friend componenet inside friends component

---
+++ src/app/user/friends/friends.component.ts
@@ -0,0 +1,23 @@
+import { UserService } from './../user.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../user.model';
+
+@Component({
+  selector: 'app-friends',
+  templateUrl: './friends.component.html',
+  styleUrls: ['./friends.component.css']
+})
+export class FriendsComponent implements OnInit {
+  @Input() friends : User[];
+  noFriends = false;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.userService.getFriends().subscribe((response: any) => {
+      this.friends = response.friends;
+      this.noFriends = this.friends.length <= 0;
+    });
+  }
+
+}
