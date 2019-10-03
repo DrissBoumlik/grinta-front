b6f38913d45a9f4dd8e157decbd4e54ea6c34447
Driss Boumlik <boumlik.t@gmail.com>
2019-10-03 16:54:27 +0100

    Fix / Add the rediretion to login page if logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -31,7 +31,7 @@ export class HeaderComponent implements OnInit {
   }
   onLogout() {
     this.loginService.logout().subscribe((response: any) => {
-      console.log(response);
+      this.router.navigate(['/']);
     });
   }
 
