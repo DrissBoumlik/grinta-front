cef56e7a13e502a8a3e8816351fdf39884dce1bc
Driss Boumlik <boumlik.t@gmail.com>
2019-10-06 01:55:07 +0100

    Add first & last name fields

--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -2,6 +2,8 @@ export class User {
   constructor(
     public id: number,
     public email: string,
-    public name: string,
+    public username: string,
+    public firstname: string,
+    public lastname: string,
     public picture: string) {}
 }
