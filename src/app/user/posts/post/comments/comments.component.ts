43d76ef331471e9d2c85b1abc32de017f1896fa6
Driss Boumlik <boumlik.t@gmail.com>
2019-10-06 02:00:21 +0100

    Add validation + Init comments array

--- src/app/user/posts/post/comments/comments.component.ts
+++ src/app/user/posts/post/comments/comments.component.ts
@@ -1,11 +1,12 @@
 import { LoginService } from 'src/app/login/login.service';
 import { CommentService } from './comment.service';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Comment } from './comment/comment.model';
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../post.model';
 import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { Subject } from 'rxjs';
+import * as jquery from 'jquery';
 
 @Component({
   selector: 'app-comments',
@@ -13,30 +14,35 @@ import { Subject } from 'rxjs';
   styleUrls: ['./comments.component.css']
 })
 export class CommentsComponent implements OnInit {
-  @Input() comments: Comment[];
+  @Input() comments: Comment[] = [];
   @Input() post: Post;
 
   CommentForm = this.fb.group({
-    content: new FormControl(null),
+    content: new FormControl(null, Validators.required),
   });
-  
+
   constructor(private fb: FormBuilder,
               private commentService: CommentService,
               private loginService: LoginService) {}
 
   ngOnInit() {
   }
-  
+
   onCreateComment() {
-    let user_id = this.loginService.user.id;
-    let post_id = this.post.id;
-    let content = this.CommentForm.get('content').value;
-    this.commentService.createComment(user_id, post_id, content)
-    .subscribe((response: any) => {
-      // this.comments.unshift(response.comment)
-      this.comments = this.commentService.addComment(this.comments, response.comment);
-    });
-    this.CommentForm.reset();
+    if (this.CommentForm.valid) {
+      let user_id = this.loginService.user.id;
+      let post_id = this.post.id;
+      let content = this.CommentForm.get('content').value;
+      this.commentService.createComment(user_id, post_id, content)
+      .subscribe((response: any) => {
+        // this.comments.unshift(response.comment)
+        this.comments = this.commentService.addComment(this.comments, response.comment);
+      });
+      this.CommentForm.reset();
+    }
+    else {
+      jQuery('#comment-' + this.post.id).addClass('ng-invalid ng-touched').removeClass('ng-untouched');
+    }
   }
 
 }
