3260b077a74a4f966db6d9872ad6640f13dacdbf
Driss Boumlik <boumlik.t@gmail.com>
2019-10-06 01:55:55 +0100

    Add infinite scroll + spinner loader

--- src/app/user/posts/posts.component.ts
+++ src/app/user/posts/posts.component.ts
@@ -8,6 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {User} from '../user.model';
 import { Post } from '../posts/post/post.model';
 import { environment } from '../../../environments/environment';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-posts',
@@ -18,11 +19,13 @@ export class PostsComponent implements OnInit {
   user: User;
   posts : Post[];
   friends : User[];
+  page = 1;
 
   constructor(private loginService: LoginService,
               private postService: PostService,
               private router: Router,
-              private http: HttpClient) { }
+              private http: HttpClient,
+              private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
     let userLogged = localStorage.getItem('_token') !== null && localStorage.getItem('_token') !== undefined;
@@ -40,11 +43,35 @@ export class PostsComponent implements OnInit {
     this.postService.postsUpdated.subscribe((posts) => {
       this.posts = posts;
     });
-    this.postService.getPosts(1).subscribe((response: any) => {
-      this.posts = response.posts;
-      this.postService.posts = response.posts;
+    this.postService.getPosts().subscribe((response: any) => {
+      this.postService.posts = this.posts = response.posts;
       console.log(this.posts);
     });
+  }
+  scroll = true;
+  gotAllPosts = false;
+  onScroll() {
+    if(this.scroll && !this.gotAllPosts) {
+      this.scroll = false;
+      this.page++;
+      this.postService.getPosts(this.page).subscribe((response: any) => {
+        /** spinner starts on init */
+        this.spinner.show();
+        setTimeout(() => {
+          /** spinner ends after 1 second = 1000ms */
+          this.spinner.hide();
+          if (!response.posts.length) {
+            this.gotAllPosts = true;
+          }
+          this.posts.push(...response.posts);
+          this.postService.posts = this.posts;
+          console.log(this.posts);
+          setTimeout(() => {
+            this.scroll = true;
+          }, 2000);
+        }, 1000);
+      });
+    }
   }
 
 }
