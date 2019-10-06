5c0afc4822b44f06a1ba52704a76a11a5a917749
Driss Boumlik <boumlik.t@gmail.com>
2019-10-06 15:56:48 +0100

    Add comment unlike functionnality

--- src/app/user/posts/post/comments/comment.service.ts
+++ src/app/user/posts/post/comments/comment.service.ts
@@ -6,19 +6,27 @@ import { Comment } from './comment/comment.model';
 
 @Injectable()
 export class CommentService {
-  
+
   headers = new HttpHeaders({
     // 'Content-Type': 'application/json',
     "Authorization": 'Bearer ' + localStorage.getItem('_token'), Accept: "application/json"
   });
   constructor(private http: HttpClient) {}
-  
+
   createComment(user_id: number, post_id: number, content: string): Observable<any> {
     return this.http.post(environment.baseApiUrl + '/comments', {user_id: user_id, post_id: post_id, content: content}, {headers: this.headers});
   }
-  
+
   addComment(comments: Comment[], comment: Comment) {
     comments.unshift(comment);
     return comments;
+  }
+
+  likeComment(comment: Comment) {
+    return this.http.post(environment.baseApiUrl + '/like-comment', {comment: comment}, {headers: this.headers});
+  }
+
+  unlikeComment(comment: Comment) {
+    return this.http.delete(environment.baseApiUrl + '/unlike-comment/' + comment.id, {headers: this.headers});
   }
 }
--- src/app/user/posts/post/comments/comment/comment.component.ts
+++ src/app/user/posts/post/comments/comment/comment.component.ts
@@ -1,5 +1,7 @@
+import { LoginService } from 'src/app/login/login.service';
 import { Comment } from './comment.model';
 import { Component, OnInit, Input } from '@angular/core';
+import { CommentService } from '../comment.service';
 
 @Component({
   selector: 'app-comment',
@@ -8,10 +10,34 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
   @Input() comment: Comment;
+  commentLiked = false;
 
-  constructor() { }
+  constructor(private loginService: LoginService,
+              private commentService: CommentService) { }
 
   ngOnInit() {
+    this.commentLiked = this.comment.likers.some((liker: any) => liker.id === this.loginService.user.id);
+  }
+
+  onLikeComment() {
+    this.commentLiked = this.comment.likers.some((liker: any) => liker.id === this.loginService.user.id);
+    if(this.commentLiked) {
+      console.log('You already like this comment');
+      this.commentService.unlikeComment(this.comment).subscribe((response: any) => {
+        console.log(response);
+        if (response.code === 200) {
+          this.comment.likers.shift();
+        }
+      });
+    } else {
+      this.commentService.likeComment(this.comment).subscribe((response: any) => {
+        console.log(response);
+        if (response.code === 200) {
+          this.comment.likers.unshift(this.loginService.user);
+        }
+      });
+    }
+    this.commentLiked = !this.commentLiked;
   }
 
 }
