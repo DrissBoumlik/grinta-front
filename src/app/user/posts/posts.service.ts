af1a4186c9bdf3ec117153c058899b2faa0e5720
Driss Boumlik <boumlik.t@gmail.com>
2020-07-18 18:12:40 +0100

    Edit posts (profile / page) url
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/posts/posts.service.ts
+++ src/app/user/posts/posts.service.ts
@@ -25,7 +25,7 @@ export class PostsService {
   getPosts(page = 1, id = null, type = null) {
     let urlParams = '';
     if (id && type) {
-      urlParams = id + '/' + type + '/';
+      urlParams = id + '/type/' + type + '/';
     }
     AuthService.getHeaders();
     return this.http.get(environment.baseApiUrl + '/posts/' + urlParams + '?page=' + page, {headers: AuthService.headers})
