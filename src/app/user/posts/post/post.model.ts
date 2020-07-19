14551cfc1b4fbb61816a09105b6bd072f2f9c77d
Driss Boumlik <boumlik.t@gmail.com>
2020-07-19 01:49:08 +0100

    Edit post header
    Add page field to the post model
    Add link if it was shared on a page
    
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/posts/post/post.model.ts
+++ src/app/user/posts/post/post.model.ts
@@ -1,14 +1,17 @@
 import { Comment } from './comments/comment/comment.model';
 import { User } from '../../user.model';
+import {Page} from '../../pages/page/page.model';
 export class Post {
   constructor(
     public id: number,
     public user_id: number,
     public post_owner_id: number,
+    public page_id: number,
     public content: string,
     public image: string,
     public user: User,
     public owner: User,
+    public page: Page,
     public likers: User[],
     public comments: Comment[],
     public created_at: string,
