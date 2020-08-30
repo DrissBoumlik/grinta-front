f9f24fdcfa5b95f2066fffb36965c0d452a7ccfd
Driss Boumlik <boumlik.t@gmail.com>
2020-08-30 17:56:38 +0100

    Implement chat functionality using firebase
    - Send message
    - Fetch previous messages
    
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/friends/chat/chat-box/chat-box.component.ts
+++ src/app/user/friends/chat/chat-box/chat-box.component.ts
@@ -4,11 +4,9 @@ import * as firebase from 'firebase';
 import {User} from '../../../user.model';
 import {ChatService} from '../../chat.service';
 import {AuthService} from '../../../../Auth/auth.service';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {DatePipe} from '@angular/common';
 
-const config = {
-  apiKey: 'AIzaSyBERjm6nvJubSHoBkkmwBDAyfb1mCL55nM',
-  databaseURL: 'https://grintaaa.firebaseio.com'
-};
 
 @Component({
   selector: 'app-chat-box',
@@ -19,32 +17,65 @@ export class ChatBoxComponent implements OnInit {
 
   @Input() user: User;
   authUser: User;
+  chats: any[] = [];
   constructor(private chatService: ChatService,
               private authService: AuthService,
+              private fb: FormBuilder,
+              public datePipe: DatePipe,
               private route: ActivatedRoute) {}
+
+  sendMessageForm = this.fb.group({
+    message: new FormControl(null, Validators.required)
+  });
 
   ngOnInit() {
 
-    firebase.initializeApp(config);
     this.authUser = this.authService.user;
     this.route.params.subscribe((params: Params) => {
       const username = params.username;
       this.chatService.getUser(username).subscribe(
         (response: any) => {
           this.user = response.user;
+          firebase.database().ref('chats/').on('value', resp => {
+            console.log(resp);
+            this.chats = [];
+            this.chats = this.snapshotToArray(resp);
+            console.log(this.chats);
+            // setTimeout(() => this.scrolltop = this.chatcontent.nativeElement.scrollHeight, 500);
+          });
         },
         (error: any) => console.log(error)
       );
     });
-    // this.chatService.userLoaded.subscribe(
-    //   (friend) => {
-    //     this.user = friend;
-    //     console.log(friend);
-    //   },
-    //   (error) => {
-    //     console.log(error);
-    //   }
-    // );
   }
 
+  snapshotToArray(snapshot: any) {
+    const returnArr = [];
+
+    snapshot.forEach((childSnapshot: any) => {
+      const item = childSnapshot.val();
+      console.log(item.date);
+      item.sender = item.from === this.user.username ? this.user : this.authUser;
+      item.key = childSnapshot.key;
+      returnArr.push(item);
+    });
+
+    return returnArr;
+  }
+
+  onSendMessage() {
+    console.log(this.sendMessageForm.value);
+    const chat = {
+      message: this.sendMessageForm.value.message,
+      date: this.datePipe.transform(new Date(), 'yyyy-MM-ddTHH:mm:ss'),
+      type: 'message',
+      from: this.authUser.username,
+      to: this.user.username,
+    };
+    const newMessage = firebase.database().ref('chats/').push();
+    newMessage.set(chat);
+    this.sendMessageForm = this.fb.group({
+      message : [null, Validators.required]
+    });
+  }
 }
