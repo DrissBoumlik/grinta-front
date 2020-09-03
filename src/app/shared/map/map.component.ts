28f4676bd5d6525e31663813b5e6ca5caaf9b027
Driss Boumlik <boumlik.t@gmail.com>
2020-09-03 04:00:03 +0100

    Implement leaflet map
    - Get event list with users
    - Draw marker for ever event location
    - Add click for user markers to navigate to chat page
    - Customize marker image with user picture
    - Edit marker style
    
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -1,91 +1,80 @@
-import {Component, ElementRef, NgZone, OnInit, ViewChild} from '@angular/core';
-import {MapsAPILoader, MouseEvent} from '@agm/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import * as L from 'leaflet';
+import {EventService} from '../event.service';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
-  title = 'AGM project';
-  latitude: number;
-  longitude: number;
-  zoom: number;
-  address: string;
-  private geoCoder;
+export class MapComponent implements OnInit, AfterViewInit {
+  private map;
+  events: [];
 
-  @ViewChild('search', {static: false})
-  public searchElementRef: ElementRef;
-
-
-  constructor(
-    private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone
-  ) { }
+  constructor(private eventService: EventService,
+              private router: Router) {
+  }
 
 
   ngOnInit() {
-    // load Places Autocomplete
-    this.mapsAPILoader.load().then(() => {
-      this.setCurrentLocation();
-      this.geoCoder = new google.maps.Geocoder;
-
-      const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
-      autocomplete.addListener('place_changed', () => {
-        this.ngZone.run(() => {
-          // get the place result
-          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-          // verify result
-          if (place.geometry === undefined || place.geometry === null) {
-            return;
-          }
-
-          // set latitude, longitude and zoom
-          this.latitude = place.geometry.location.lat();
-          this.longitude = place.geometry.location.lng();
-          this.zoom = 12;
-        });
-      });
-    });
   }
 
-  // Get Current Location Coordinates
-  private setCurrentLocation() {
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [48.8494709, 2.3468044],
+      zoom: 14
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
     if ('geolocation' in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.zoom = 8;
-        this.getAddress(this.latitude, this.longitude);
+        console.log(position);
       });
     }
-  }
 
-
-  markerDragEnd($event: MouseEvent) {
-    console.log($event);
-    this.latitude = $event.coords.lat;
-    this.longitude = $event.coords.lng;
-    this.getAddress(this.latitude, this.longitude);
-  }
-
-  getAddress(latitude, longitude) {
-    this.geoCoder.geocode({ location: { lat: latitude, lng: longitude } }, (results, status) => {
-      console.log(results);
-      console.log(status);
-      if (status === 'OK') {
-        if (results[0]) {
-          this.zoom = 12;
-          this.address = results[0].formatted_address;
-        } else {
-          window.alert('No results found');
-        }
-      } else {
-        window.alert('Geocoder failed due to: ' + status);
+    this.eventService.getEvents().subscribe(
+      (response: any) => {
+        console.log(response);
+        this.events = response.events;
+        this.events.forEach((event: any) => {
+          const location = event.location.split(',');
+          const items = [1, 2, 3, 4];
+          let index = Math.floor(Math.random() * items.length);
+          if (index >= items.length) {
+            index = items.length - 1;
+          }
+          L.marker(location, {
+            icon: L.icon({
+              iconSize: [30, 30],
+              iconAnchor: [13, 37],
+              iconUrl: '/assets/images/leaflet/me-marker-' + items[index] + '.jpg',
+              // event.user.picture, // '/assets/images/leaflet/marker-icon.png',
+              // shadowUrl: '/assets/images/leaflet/marker-shadow.png'
+              className: 'map-icon-marker'
+            })
+          }).on('click', (e) => {
+            console.log(e);
+            console.log(event.user.username);
+            this.router.navigate(['/messenger/' + event.user.username]);
+          }).addTo(this.map);
+        });
+      },
+      (error) => {
+        console.log(error);
       }
+    );
 
-    });
   }
 
 }
