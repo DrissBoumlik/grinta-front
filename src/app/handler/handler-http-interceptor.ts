bd186275d80b879fd0210588b6d3f518cca48ace
Driss Boumlik <boumlik.t@gmail.com>
2020-09-11 02:11:47 +0100

    Edit http interceptor logic
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/handler/handler-http-interceptor.ts
+++ src/app/handler/handler-http-interceptor.ts
@@ -1,23 +1,46 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
-import {catchError} from 'rxjs/operators';
+import {catchError, tap} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import {AuthService} from '../Auth/auth.service';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class HandlerHttpInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private router: Router) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(req).pipe(
-      catchError((error: any) => {
-        console.log(error);
-        if (error.status === 401) {
-          this.authService.logout();
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authUser = JSON.parse(localStorage.getItem('authUser'));
+    const token = localStorage.getItem('token');
+    if (authUser && token) {
+      request = request.clone({
+        setHeaders: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + token
         }
-        return throwError(error);
-      })
-    );
+      });
+    }
+
+    return next.handle(request).pipe( tap(() => {},
+      (err: any) => {
+        if (err instanceof HttpErrorResponse) {
+          if (err.status !== 401) {
+            return;
+          }
+          this.authService.logout({serverSide: false});
+        }
+      }));
+
+    // return next.handle(req).pipe(
+    //   catchError((error: any) => {
+    //     console.log(error);
+    //     if (error.status === 401) {
+    //       this.authService.logout();
+    //     }
+    //     return throwError(error);
+    //   })
+    // );
   }
 }
