2b029cad282ccfb8aefff3f3b0f6ffcb3f33f4a6
Driss Boumlik <boumlik.t@gmail.com>
2020-09-12 00:50:17 +0100

    Add event options component
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

---
+++ src/app/user/events/event-options/event-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-options',
+  templateUrl: './event-options.component.html',
+  styleUrls: ['./event-options.component.css']
+})
+export class EventOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
