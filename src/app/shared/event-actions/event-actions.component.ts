4b1842217189cee9a7716419a1c599480032d10e
Driss Boumlik <boumlik.t@gmail.com>
2020-09-12 00:49:55 +0100

    Add event action component
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

---
+++ src/app/shared/event-actions/event-actions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-action',
+  templateUrl: './event-actions.component.html',
+  styleUrls: ['./event-actions.component.css']
+})
+export class EventActionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
