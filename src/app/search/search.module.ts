938f6e97b20c3cefa783fb3dc686f8fdb6d8f32b
Driss Boumlik <boumlik.t@gmail.com>
2020-09-12 22:06:21 +0100

    Move search module to the root
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {SharedModule} from '../shared/shared.module';
 import {SearchComponent} from './search.component';
 import {SearchEventsComponent} from './search-events/search-events.component';
-import {SharedModule} from '../../shared/shared.module';
 
 
 
