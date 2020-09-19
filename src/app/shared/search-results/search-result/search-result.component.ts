af77915556cf45719abbbf60d71d6e147dfb088e
Driss Boumlik <boumlik.t@gmail.com>
2020-09-19 16:01:47 +0100

    Add title to search results
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/shared/search-results/search-result/search-result.component.ts
+++ src/app/shared/search-results/search-result/search-result.component.ts
@@ -10,18 +10,23 @@ import {Router} from '@angular/router';
 export class SearchResultComponent implements OnInit {
   @Input() result: {header: string, image: string, link: string, type: string};
   iconType: string;
+  title: string;
 
   constructor(private searchService: SearchService,
               private router: Router) {}
 
   ngOnInit() {
     if (this.result.type === 'user') {
+      this.title = 'Utilisateur';
       this.iconType = '<div class="user iq-bg-danger"><i class="ri-user-line"></i></div>';
     } else if (this.result.type === 'post') {
+      this.title = 'Post';
       this.iconType = '<div class="page iq-bg-dark"><i class="ri-article-line"></i></div>';
     } else if (this.result.type === 'page') {
+      this.title = 'Page';
       this.iconType = '<div class="page iq-bg-primary"><i class="ri-pages-line"></i></div>';
     } else if (this.result.type === 'event') {
+      this.title = 'Evenement';
       this.iconType = '<div class="event iq-bg-warning"><i class="ri-calendar-event-line"></i></div>';
     }
   }
