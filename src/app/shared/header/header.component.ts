f1cbbf1108dae8b15d2a973545f3ba99bf492cbd
Driss Boumlik <boumlik.t@gmail.com>
2020-09-21 18:33:41 +0100

    Implement notification listening example: (event created)
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../auth/auth.service';
 import {User} from '../../user/user.model';
 import {Router} from '@angular/router';
+import Echo from 'laravel-echo';
 
 @Component({
   selector: 'app-header',
@@ -10,12 +11,30 @@ import {Router} from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   user: User;
+  notify = false;
+  showNotifications = false;
 
   constructor(private authService: AuthService,
               private router: Router) { }
 
   ngOnInit() {
     this.user = this.authService.user;
+    const token = localStorage.getItem('token');
+    const echo = new Echo({
+      broadcaster: 'socket.io',
+      host: window.location.hostname + ':6001',
+      auth: {headers: {Authorization: 'Bearer ' + token}}
+    });
+    localStorage.setItem('socketID', JSON.stringify(echo.socketId()));
+    console.log(echo.socketId());
+    let channel = '';
+    channel = 'event.created';
+    echo.private(channel)
+      .listen('.Event', (e) => {
+        this.notify = true;
+        console.log(e);
+      });
+
     // const token = localStorage.getItem('token');
     // const logged = (this.authService.user !== null && this.authService.user !== undefined) && (token !== undefined && token !== null);
     // if (logged) {
@@ -30,4 +49,8 @@ export class HeaderComponent implements OnInit {
     });
   }
 
+  onShowNotifications() {
+    this.showNotifications = !this.showNotifications;
+    this.notify = false;
+  }
 }
