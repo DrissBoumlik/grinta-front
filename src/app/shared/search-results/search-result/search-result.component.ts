378b46dc0765c808fc91a5acbc63deac29df1db2
Driss Boumlik <boumlik.t@gmail.com>
2020-09-24 18:33:32 +0100

    Add icons type to search results (post, page, user, event, ...)
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/shared/search-results/search-result/search-result.component.ts
+++ src/app/shared/search-results/search-result/search-result.component.ts
@@ -18,16 +18,16 @@ export class SearchResultComponent implements OnInit {
   ngOnInit() {
     if (this.result.type === 'user') {
       this.title = 'Utilisateur';
-      this.iconType = '<span class="user"><i class="ri-user-line"></i></span>';
+      this.iconType = '<span class="user"><i class="iq-bg-danger ri-user-line"></i></span>';
     } else if (this.result.type === 'post') {
       this.title = 'Post';
-      this.iconType = '<span class="page"><i class="ri-article-line"></i></span>';
+      this.iconType = '<span class="page"><i class="iq-bg-dark ri-article-line"></i></span>';
     } else if (this.result.type === 'page') {
       this.title = 'Page';
-      this.iconType = '<span class="page"><i class="ri-pages-line"></i></span>';
+      this.iconType = '<span class="page"><i class="iq-bg-primary ri-pages-line"></i></span>';
     } else if (this.result.type === 'event') {
       this.title = 'Evenement';
-      this.iconType = '<span class="event"><i class="ri-calendar-event-line"></i></span>';
+      this.iconType = '<span class="event"><i class="iq-bg-warning ri-calendar-event-line"></i></span>';
     }
   }
 
