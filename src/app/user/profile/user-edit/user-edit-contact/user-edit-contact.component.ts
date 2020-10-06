c4f06587c9a8d28051906e466d81e343461c3ced
Driss Boumlik <boumlik.t@gmail.com>
2020-10-06 01:15:08 +0100

    Implement user contact data edition
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/profile/user-edit/user-edit-contact/user-edit-contact.component.ts
+++ src/app/user/profile/user-edit/user-edit-contact/user-edit-contact.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../../user.model';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProfileService} from '../../profile.service';
+import {FeedbackService} from '../../../../shared/feedback/feedback.service';
 
 @Component({
   selector: 'app-user-edit-contact',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditContactComponent implements OnInit {
 
-  constructor() { }
+  profile: User;
+
+  editUserForm = new FormGroup({
+    phone_number: new FormControl(null, Validators.required),
+    email: new FormControl(null, Validators.required),
+  });
+  constructor(private profileService: ProfileService,
+              private feedbackService: FeedbackService) { }
 
   ngOnInit() {
+    this.profileService.profileLoaded.subscribe((profile: User) => {
+      this.profile = profile;
+      this.initForm();
+    });
+    this.profile = this.profileService.profile;
+    this.initForm();
+  }
+
+  initForm() {
+    this.editUserForm.patchValue({
+      phone_number: this.profile.phoneNumber,
+      email: this.profile.email,
+    });
+  }
+
+  onEdit() {
+    const profile = {
+      phone_number: this.editUserForm.value.phone_number,
+      email: this.editUserForm.value.email,
+    };
+
+    this.profileService.updateContact(profile)
+      .subscribe((response: any) => {
+          this.feedbackService.feedbackReceived.next({feedback: 'success', message: response.message});
+        },
+        (error: any) => {
+          console.log(error);
+          const message = error.error.errors ? error.error.errors : error.error.message;
+          this.feedbackService.feedbackReceived.next({feedback: 'error', message});
+        }
+      );
   }
 
 }
