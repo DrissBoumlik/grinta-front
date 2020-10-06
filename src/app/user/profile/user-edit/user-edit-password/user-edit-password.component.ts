e71581b9fd52a91880ec06b2f0d85d4376b94d8d
Driss Boumlik <boumlik.t@gmail.com>
2020-10-06 01:15:38 +0100

    Implement user password edition
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/profile/user-edit/user-edit-password/user-edit-password.component.ts
+++ src/app/user/profile/user-edit/user-edit-password/user-edit-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ProfileService} from '../../profile.service';
+import {FeedbackService} from '../../../../shared/feedback/feedback.service';
+import {User} from '../../../user.model';
 
 @Component({
   selector: 'app-user-edit-password',
@@ -7,9 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditPasswordComponent implements OnInit {
 
-  constructor() { }
+  profile: User;
+
+  editUserForm = new FormGroup({
+    password_old: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+    password_confirmation: new FormControl(null, Validators.required),
+  });
+  constructor(private profileService: ProfileService,
+              private feedbackService: FeedbackService) { }
 
   ngOnInit() {
+    this.profileService.profileLoaded.subscribe((profile: User) => {
+      this.profile = profile;
+    });
+    this.profile = this.profileService.profile;
   }
 
+  onEdit() {
+    const profile = {
+      password_old: this.editUserForm.value.password_old,
+      password: this.editUserForm.value.password,
+      password_confirmation: this.editUserForm.value.password_confirmation,
+    };
+
+    this.profileService.updatePassword(profile)
+      .subscribe((response: any) => {
+          this.profileService.getProfile(response.user.username).subscribe((data: any) => {
+            this.profileService.profileUpdated.next(data.user);
+          });
+
+          this.feedbackService.feedbackReceived.next({feedback: 'success', message: response.message});
+        },
+        (error: any) => {
+          console.log(error);
+          const message = error.error.errors ? error.error.errors : error.error.message;
+          this.feedbackService.feedbackReceived.next({feedback: 'error', message});
+        }
+      );
+  }
 }
