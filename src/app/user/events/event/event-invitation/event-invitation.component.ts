3a163259e65df4a192a109b209df2764eac98d94
Driss Boumlik <boumlik.t@gmail.com>
2020-10-07 13:11:20 +0100

    Close modal on click outside
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/events/event/event-invitation/event-invitation.component.ts
+++ src/app/user/events/event/event-invitation/event-invitation.component.ts
@@ -1,11 +1,11 @@
-import {Component, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, OnInit, Output, Renderer2, ViewChild} from '@angular/core';
 import {from, Subject} from 'rxjs';
 import {RelationService} from '../../../friends/relation.service';
 import {User} from '../../../user.model';
 import {EventService} from '../../../../shared/event.service';
 import {pluck} from 'rxjs/operators';
-import {Event} from "../../event.model";
-import {FeedbackService} from "../../../../shared/feedback/feedback.service";
+import {Event as EventModel} from '../../event.model';
+import {FeedbackService} from '../../../../shared/feedback/feedback.service';
 
 @Component({
   selector: 'app-event-invitation',
@@ -13,20 +13,29 @@ import {FeedbackService} from "../../../../shared/feedback/feedback.service";
   styleUrls: ['./event-invitation.component.css']
 })
 export class EventInvitationComponent implements OnInit {
-  event: Event;
+  @ViewChild('modal', {static: false}) modal: ElementRef;
+  @ViewChild('modalWrapper', {static: false}) modalWrapper: ElementRef;
+  event: EventModel;
   noRelations = false;
   relations: any[] = [];
   @Output() closeModal = new Subject();
   constructor(private relationService: RelationService,
               private feedbackService: FeedbackService,
+              private renderer: Renderer2,
               private eventService: EventService) { }
 
   ngOnInit() {
-    this.eventService.eventLoaded.subscribe((event: Event) => {
+    this.eventService.eventLoaded.subscribe((event: EventModel) => {
       this.event = event;
     });
     this.event = this.eventService.event;
     this.onGetRelations();
+
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (this.modalWrapper && e.target !== this.modal.nativeElement && e.target === this.modalWrapper.nativeElement) {
+        this.onCloseModal();
+      }
+    });
   }
 
   onGetRelations() {
