4f7c9ed2e1bb79ca22d85136d1267b26cc1e8595
Driss Boumlik <boumlik.t@gmail.com>
2020-10-14 09:24:11 +0100

    Implement notifications in cas of new messages (WIP)
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -16,12 +16,15 @@ export class HeaderComponent implements OnInit {
   alertNewNotification = false;
   alertNewMessage = false;
   showNotifications = false;
+  showChatList = false;
   notifications: any[] = [];
   newNotifications: any[] = [];
-  emptyList = false;
+  emptyNotificationsList = false;
+  emptyChatList = false;
   fbDB = firebase.database();
   chatDB = 'chats';
   chatList = [];
+  newChatList = [];
 
   constructor(private authService: AuthService,
               private eventFeedbackService: EventFeedbackService,
@@ -32,7 +35,7 @@ export class HeaderComponent implements OnInit {
 
     this.setUpNotifications();
 
-    this.setupChtNotifications();
+    this.setupChatNotifications();
 
     // const token = localStorage.getItem('token');
     // const logged = (this.authService.user !== null && this.authService.user !== undefined) && (token !== undefined && token !== null);
@@ -43,42 +46,98 @@ export class HeaderComponent implements OnInit {
     // }
   }
 
-  setupChtNotifications() {
+  onShowChatList() {
+    this.showChatList = !this.showChatList;
+    this.alertNewMessage = false;
+    if (this.showChatList) {
+      this.newChatList = [];
+      // this.notifications = this.notifications.map((notification) => {
+      //   notification.seen = true;
+      //   return notification;
+      // });
+      // localStorage.setItem('notifications', JSON.stringify(this.notifications));
+      localStorage.setItem('chatList', JSON.stringify(this.chatList));
+    }
+  }
+
+  setupChatNotifications() {
+    this.chatList = JSON.parse(localStorage.getItem('chatList'));
+    if (!this.chatList) {
+      this.newChatList = [];
+      this.chatList = [];
+    }
+    this.emptyChatList = !this.chatList.length;
 
     this.fbDB.ref(this.chatDB).on('value', resp => {
       const results = this.snapshotToArray(resp);
-      this.chatList = results.chats;
+      // this.chatList = results.chats;
     });
   }
 
 
   snapshotToArray(snapshot: any) {
     const returnArr = [];
-    const chats = [];
-
     snapshot.forEach((childSnapshot: any) => {
       const items: any = Object.values(childSnapshot.val());
       const lastItem = items[items.length - 1];
-      const otherUsername = this.user.username === lastItem.fromUsername ? lastItem.toUsername : lastItem.fromUsername;
-      const otherPicture = this.user.username === lastItem.fromUsername ? lastItem.toPicture : lastItem.fromPicture;
-      const otherFirstname = this.user.firstname === lastItem.fromFirstname ? lastItem.toFirstname : lastItem.fromFirstname;
-      const otherLastname = this.user.lastname === lastItem.fromLastname ? lastItem.toLastname : lastItem.fromLastname;
       const offsetDate = this.timeDifference(new Date(), Date.parse(lastItem.date));
-      const chat = {
-        username: otherUsername,
-        firstname: otherFirstname,
-        lastname: otherLastname,
+      let chat: any = {
         lastMsg: lastItem.message,
-        picture: otherPicture,
-        offsetDate // lastItem.date
+        dateMsg: lastItem.date,
+        offsetDate, // lastItem.date
+        details: {
+          fromUsername: lastItem.fromUsername,
+          toUsername: lastItem.toUsername
+        }
       };
-      if (this.user.username === lastItem.fromUsername || this.user.username === lastItem.toUsername) {
-        chats.push(chat);
-        returnArr.push(chat.username);
+      if (this.user.username === lastItem.fromUsername) {
+        chat = {
+          username: lastItem.toUsername,
+          firstname: lastItem.toFirstname,
+          lastname: lastItem.toLastname,
+          picture: lastItem.toPicture,
+          ...chat,
+        };
+      } else {
+        chat = {
+          username: lastItem.fromUsername,
+          firstname: lastItem.fromFirstname,
+          lastname: lastItem.fromLastname,
+          picture: lastItem.fromPicture,
+          ...chat
+        };
+      }
+      const exists = this.chatList.some((chatItem) => {
+        return chatItem.details.fromUsername === chat.details.fromUsername &&
+          chatItem.details.toUsername === chat.details.toUsername &&
+          chatItem.dateMsg === chat.dateMsg;
+      });
+      if (!exists) {
+        if (this.user.username === lastItem.fromUsername || this.user.username === lastItem.toUsername) {
+          this.newChatList.push(chat);
+          returnArr.push(chat.username);
+        }
       }
     });
-
-    return {userNames: returnArr, chats};
+    // TODO: Update message instead of adding to the list if same conversation
+    // this.alertNewMessage = !!this.newChatList.length;
+    if (this.newChatList.length && this.chatList.length) {
+      this.alertNewMessage = !(this.newChatList[0].details.fromUsername === this.chatList[0].details.fromUsername &&
+        this.newChatList[0].details.toUsername === this.chatList[0].details.toUsername &&
+        this.newChatList[0].dateMsg === this.chatList[0].dateMsg);
+    }
+    this.chatList.unshift(...this.newChatList);
+    this.chatList = this.chatList.sort((chatItem1, chatItem2) => {
+      if (chatItem1.dateMsg > chatItem2.dateMsg) {
+        return -1;
+      }
+      if (chatItem1.dateMsg < chatItem2.dateMsg) {
+        return 1;
+      }
+      return 0;
+    });
+    this.chatList = this.chatList.slice(0, 5);
+    return {userNames: returnArr, chats: this.chatList};
   }
 
   setUpNotifications() {
@@ -86,9 +145,9 @@ export class HeaderComponent implements OnInit {
     if (!this.notifications) {
       this.notifications = this.newNotifications = [];
     }
-    this.emptyList = !this.notifications.length;
+    this.emptyNotificationsList = !this.notifications.length;
     this.alertNewNotification = this.notifications.some((notification) => {
-        return !notification.seen;
+      return !notification.seen;
     });
     this.updateNewNotifications();
     const token = localStorage.getItem('token');
@@ -141,7 +200,7 @@ export class HeaderComponent implements OnInit {
     this.newNotifications.unshift(notification);
 
     this.notifications = this.notifications.slice(0, 5);
-    this.emptyList = !this.notifications.length;
+    this.emptyNotificationsList = !this.notifications.length;
     localStorage.setItem('notifications', JSON.stringify(this.notifications));
   }
 
@@ -159,14 +218,14 @@ export class HeaderComponent implements OnInit {
       return Math.round(elapsed / 1000) + ' seconds ago';
     } else if (elapsed < msPerHour) {
       return Math.round(elapsed / msPerMinute) + ' minutes ago';
-    } else if (elapsed < msPerDay ) {
-      return Math.round(elapsed / msPerHour ) + ' hours ago';
+    } else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + ' hours ago';
     } else if (elapsed < msPerMonth) {
       return 'approximately ' + Math.round(elapsed / msPerDay) + ' days ago';
     } else if (elapsed < msPerYear) {
       return 'approximately ' + Math.round(elapsed / msPerMonth) + ' months ago';
     } else {
-      return 'approximately ' + Math.round(elapsed / msPerYear ) + ' years ago';
+      return 'approximately ' + Math.round(elapsed / msPerYear) + ' years ago';
     }
   }
 
