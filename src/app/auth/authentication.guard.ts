b2db8a2c432e782bfaf5cc7e53b51940e8a45d79
Driss Boumlik <boumlik.t@gmail.com>
2020-10-19 21:19:49 +0100

    Clean code
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/auth/authentication.guard.ts
+++ src/app/auth/authentication.guard.ts
@@ -10,10 +10,6 @@ export class AuthenticationGuard implements CanActivate {
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot)
     : Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    console.log('============= guard =============');
-    console.log(this.authService);
-    console.log(this.authService.user);
-    console.log(this.authService.isLogged);
     if (!this.authService.isLogged) {
       this.authService.logout({serverSide: false});
     }
