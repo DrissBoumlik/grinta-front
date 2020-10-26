522c31b9f7661a58ef7786b5c126a95944271d64
Driss Boumlik <boumlik.t@gmail.com>
2020-10-26 19:50:06 +0100

    Add albums section on profile
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/profile/user-photos/user-photos.component.ts
+++ src/app/user/profile/user-photos/user-photos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from '../../user.model';
 import {ProfileService} from '../profile.service';
+import {Album} from '../../albums/album/album.model';
 
 @Component({
   selector: 'app-user-photos',
@@ -9,16 +10,36 @@ import {ProfileService} from '../profile.service';
 })
 export class UserPhotosComponent implements OnInit {
   profile: User;
+  albums: (Album | any)[];
   emptyList = false;
+  emptyAlbums = false;
   constructor(private profileService: ProfileService) { }
 
   ngOnInit() {
     this.profileService.profileLoaded.subscribe((profile: User) => {
       this.profile = profile;
-      this.emptyList = !this.profile.photos.length;
+      this.init();
     });
     this.profile = this.profileService.profile;
+    this.init();
+  }
+
+  init() {
     this.emptyList = !this.profile.photos.length;
+    this.emptyAlbums = !this.profile.albums.length;
+    this.albums = this.profile.albums.filter((album) => {
+      return album.medias.length;
+    });
+    this.albums = this.albums.map((album: Album | any) => {
+      let coverAlbum = '/assets/images/default/album-cover.jpg';
+      for (const media of album.medias) {
+        if (media.type === 'image') {
+          coverAlbum = media.media_url;
+        }
+      }
+      album.cover = coverAlbum;
+      return album;
+    });
   }
 
 }
--- src/app/user/user.model.ts
+++ src/app/user/user.model.ts
@@ -43,7 +43,7 @@ export class User {
     public likedPages: Page[],
     public medias: Media[],
     public photos: Media[],
-    public albums: Album[],
+    public albums: (Album | any)[],
     public sports: Sport[]
   ) {}
 }
