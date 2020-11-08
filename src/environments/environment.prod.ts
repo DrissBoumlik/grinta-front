0d6eb46a9cd08a3090ecc49355fcb00aa62de8d7
Driss Boumlik <boumlik.t@gmail.com>
2020-11-08 14:27:35 +0100

    Add env params to prod file
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   baseApiUrl: 'https://api.grintaaa.essorline.com/api',
-  baseUrl: 'https://api.grintaaa.essorline.com/'
+  baseUrl: 'https://api.grintaaa.essorline.com/',
+  maxWidthCover: 1000,
+  maxHeightCover: 400,
 };
