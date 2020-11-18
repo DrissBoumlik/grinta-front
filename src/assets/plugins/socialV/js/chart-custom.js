92e328adc87b3727dd0f7d6702cf0253f9364832
Driss Boumlik <boumlik.t@gmail.com>
2020-11-18 23:31:52 +0100

    Fix right sidebar toggling + Edit friends side icon
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/assets/plugins/socialV/js/chart-custom.js
+++ src/assets/plugins/socialV/js/chart-custom.js
@@ -6,8 +6,9 @@
 		t ? e("body").addClass("right-sidebar-close") : e("body").removeClass("right-sidebar-close")
 	}
 	e(document).ready(function() {
-		var a = !1;
-		t(a), e(document).on("click", ".right-sidebar-toggle", function() {
+		var a = 1;
+		// t(a),
+      e(document).on("click", ".right-sidebar-toggle", function() {
 			t(a = !a)
 		})
 	})
