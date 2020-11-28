b6b7041641d2f0b25329f09efb23c681e8c80d4d
Driss Boumlik <boumlik.t@gmail.com>
2020-11-28 02:11:02 +0100

    Hide new post form on different profile than auth user
    Signed-off-by: Driss Boumlik <boumlik.t@gmail.com>

--- src/app/user/profile/profile-posts/profile-posts.component.ts
+++ src/app/user/profile/profile-posts/profile-posts.component.ts
@@ -4,6 +4,7 @@ import {User} from '../../user.model';
 import {ProfileService} from '../profile.service';
 import {NgxSpinnerService} from 'ngx-spinner';
 import {PostsService} from '../../posts/posts.service';
+import {AuthService} from '../../../auth/auth.service';
 
 @Component({
   selector: 'app-profile-posts',
@@ -12,9 +13,12 @@ import {PostsService} from '../../posts/posts.service';
 })
 export class ProfilePostsComponent implements OnInit {
   user: User;
+  authUser: User;
+  ownProfile = false;
   justLoaded = false;
 
   constructor(private userService: UserService,
+              private authService: AuthService,
               private postsService: PostsService,
               private profileService: ProfileService) { }
 
@@ -24,10 +28,12 @@ export class ProfilePostsComponent implements OnInit {
     //   this.justLoaded = true;
     //   this.postsService.getPosts(1, this.profile.id, 'user');
     // }
+    this.authUser = this.authService.user;
 
     this.profileService.profileLoaded.subscribe((user: User) => {
       this.user = user;
       this.justLoaded = true;
+      this.ownProfile = this.authUser.uuid === this.user.uuid;
       // this.postsService.getPosts(1, this.profile.id, 'user');
     });
   }
