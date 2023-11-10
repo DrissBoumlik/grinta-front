3277996482ab5f15f8c697266780ab8a13748d57
Driss <drissboumlik.dev@gmail.com>
2023-11-10 16:24:38 +0100

    Add extra data to firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,13 @@ export const environment = {
   clientUrl: 'https://drissboumlik.github.io/grinta-front/',
   maxWidthCover: 1000,
   maxHeightCover: 400,
+  firebase: {
+    apiKey: 'AIzaSyBERjm6nvJubSHoBkkmwBDAyfb1mCL55nM',
+    authDomain: 'grintaaa.firebaseapp.com',
+    projectId: 'grintaaa',
+    storageBucket: 'grintaaa.appspot.com',
+    messagingSenderId: '52047750060',
+    appId: '1:52047750060:web:af335191bf0e8e2ffcd009',
+    databaseURL: 'https://grintaaa.firebaseio.com'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,13 @@ export const environment = {
   clientUrl: 'http://127.0.0.1:4200',
   maxWidthCover: 1000,
   maxHeightCover: 400,
-  firebase : {
+  firebase: {
     apiKey: 'AIzaSyBERjm6nvJubSHoBkkmwBDAyfb1mCL55nM',
+    authDomain: 'grintaaa.firebaseapp.com',
+    projectId: 'grintaaa',
+    storageBucket: 'grintaaa.appspot.com',
+    messagingSenderId: '52047750060',
+    appId: '1:52047750060:web:af335191bf0e8e2ffcd009',
     databaseURL: 'https://grintaaa.firebaseio.com'
   }
 };
