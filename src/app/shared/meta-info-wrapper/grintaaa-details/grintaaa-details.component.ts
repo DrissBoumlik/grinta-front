f01445d30cd3c9a85a94a564b5f53e4f03462fec
Driss <drissboumlik.dev@gmail.com>
2023-11-12 00:01:14 +0100

    Edit brand text

--- src/app/shared/meta-info-wrapper/grintaaa-details/grintaaa-details.component.ts
+++ src/app/shared/meta-info-wrapper/grintaaa-details/grintaaa-details.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-grintaaa-details',
@@ -10,6 +11,7 @@ export class GrintaaaDetailsComponent implements OnInit {
   links: {text: string, href: string}[];
 
   constructor() {}
+  protected readonly environment = environment;
 
   ngOnInit(): void {
     this.links = [
