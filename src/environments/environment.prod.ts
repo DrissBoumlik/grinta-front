7e29bc8c8b13ad204cfebe2e953b3a32f6fc8e08
Driss <drissboumlik.dev@gmail.com>
2024-02-01 00:01:07 +0100

    Edit backend api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://projects.drissboumlik.com/grinta-back/public/api',
-  baseUrl: 'https://projects.drissboumlik.com/grinta-back/public/',
+  baseApiUrl: 'https://grintaaa.drissboumlik.com/api',
+  baseUrl: 'https://grintaaa.drissboumlik.com/',
   clientUrl: 'https://drissboumlik.github.io/grinta-front/',
   maxWidthCover: 1000,
   maxHeightCover: 400,
